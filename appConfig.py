32ca911f9def24f3037ea137e1651ee712b38325
Pranil Kharche <pranilkharche@gmail.com>
2022-07-24 19:57:15 +0530

    AppConfig added

---
+++ appConfig.py
@@ -0,0 +1,23 @@
+CONFIG={
+    'about':{
+        'name':'certisign',
+        'version':'1.0.0',
+        'description':'Certificate Signing Application',
+        'author':'Pranil Kharche',
+        'email':'pranilkharche@gmail.com',
+        'url':'',
+        'license':'MIT',
+    },
+    'database':{
+        'host':'localhost',
+        'port':27017,
+        'name':'certisign',
+        'username':'',  
+        'password':'',
+    },
+    'server':{
+        'host':'',
+        'port':8080,
+        'debug':True,
+    },
+}
